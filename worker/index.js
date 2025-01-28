50ac2e33756a72b08dc4ea0186b64195a741dbf9
John Anchery <john.anchery@mail.ru>
2025-01-28 21:25:57 +0530

    fetch daemon job created

--- worker/index.js
+++ worker/index.js
@@ -1,9 +1,24 @@
 export default {
-  async fetch(request) {
-    const data = {
-      hello: "world",
-    };
-
-    return Response.json(data);
+  async fetch(request, env) {
+    const { pathname } = new URL(request.url);
+    if (pathname === "/api/fetch-daemon-job") {
+        const data = await env.DB.prepare(
+          "SELECT command FROM runnable WHERE status = ?"
+        ).bind('CREATED').all();
+        await env.DB.prepare(
+            "UPDATE runnable SET status='SENT TO DAEMON' WHERE status = ?"
+        ).bind('CREATED').all()
+        const response = {
+            statusCode: 200,
+            body: data.results,
+        };
+        return Response.json(response);
+    } else {
+        const response = {
+            statusCode: 204,
+            body: 'Use /api/fetch-daemon-job',
+        };
+        return Response.json(response);
+    }
   },
 };
